2a2ee62623033967b44cf2eec7fb72e226cb559c
Kaos <kaoppenh@ucsc.edu>
2016-01-22 19:37:52 -0800

    adding save/load with standard application

---
+++ ComaLoadSave/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	public Font pixel;
+
+	void OnGUI ()
+	{
+		GUIStyle pixelStyle = new GUIStyle(GUI.skin.button);
+		pixelStyle.fontSize = 10;
+		pixelStyle.font = pixel;
+
+		if (GUI.Button (new Rect (50, 20, 100, 30), "Level1", pixelStyle))
+			StartCoroutine ("FadeToNextLevel", "savestuff");
+
+		if(GUI.Button(new Rect(200, 20, 100, 30), "Level2", pixelStyle))
+			StartCoroutine ("FadeToNextLevel", "TestSave");
+
+		if(GUI.Button(new Rect(350, 20, 100, 30), "StartMenu", pixelStyle))
+			StartCoroutine ("FadeToNextLevel", "LoadMenu");
+	}
+
+	//coroutine to fade between scenes
+	IEnumerator FadeToNextLevel(string scene){
+		float fadetime = GameObject.Find ("SaveData").GetComponent<FadingScenes> ().BeginFade (1);
+		yield return new WaitForSeconds(fadetime);
+		Application.LoadLevel(scene);
+	}
+}
---
+++ ComaLoadSave/Assets/Scripts/SelectionHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SelectionHandler
+{
+	private int selectedIndex;
+	private string optionText;
+
+	private List<string> optionLabels = new List<string>();
+
+	public SelectionHandler(List<string> labels)
+	{
+		selectedIndex = 0;
+		optionLabels.Clear ();
+		optionLabels = labels;
+		optionText = optionLabels [0];
+	}
+
+	public void Reset()
+	{
+		selectedIndex = 0;
+	}
+
+	public void Next()
+	{
+		selectedIndex = selectedIndex > 0 ? selectedIndex-1 : optionLabels.Count - 1;
+		optionText = optionLabels [selectedIndex];
+	}
+
+	public void Previous(){
+		selectedIndex = (selectedIndex + 1) % optionLabels.Count;
+		optionText = optionLabels [selectedIndex];
+	}
+
+	public void ChangeListItem(int index, string value){
+		optionLabels[index] = value;
+	}
+
+	public int GetListSize(){
+		return optionLabels.Count;
+	}
+
+	public string GetOptionListString()
+	{
+		return optionText;
+	}
+
+	public int GetSelectedIndex()
+	{
+		return selectedIndex;
+	}
+}
