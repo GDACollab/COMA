de4695b096192568860e10fbb94beac20b526bb8
Kaos <kaoppenh@ucsc.edu>
2016-01-22 18:30:33 -0800

    removed background of health bar

--- COMA assets/Assets/life.cs
+++ COMA assets/Assets/life.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class life : MonoBehaviour {
 
 	public Rect lifebarRect;
-	public Rect lifebarBGRect;
+	//public Rect lifebarBGRect;
 	public Texture2D lifebar;
-	public Texture2D lifebarBG;
+	//public Texture2D lifebarBG;
 	//private Health heart;
 	//public GameObject PlayerData;
 	private float width = 300f;
@@ -26,9 +26,9 @@ public class life : MonoBehaviour {
 	void OnGUI(){
 		instance.lifebarRect.width = width * (Health.hp / 100);
 		instance.lifebarRect.height = 10;
-		instance.lifebarBGRect.width = width;
-		instance.lifebarBGRect.height = 10;
-		GUI.DrawTexture (lifebarBGRect, lifebarBG);
+		//instance.lifebarBGRect.width = width;
+		//instance.lifebarBGRect.height = 10;
+		//GUI.DrawTexture (lifebarBGRect, lifebarBG);
 		GUI.DrawTexture (lifebarRect, lifebar);
 	}
 }
