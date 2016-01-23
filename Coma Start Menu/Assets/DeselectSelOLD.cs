10be66a7b3f2d482f87a4f295445808de1b6b8e0
Ethan <ethanmmwong@gmail.com>
2016-01-23 01:13:15 -0800

    work

---
+++ Coma Start Menu/Assets/DeselectSelOLD.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeselectSel : MonoBehaviour {
+
+	private Vector3 tempV;
+	// Use this for initialization
+	void Start () {
+		tempV = new Vector3 (0, 0, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void hit() {
+		this.gameObject.transform.localPosition += tempV;
+	}
+
+	public void unhit() {
+		this.gameObject.transform.localPosition -= tempV;
+	}
+}
