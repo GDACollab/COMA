cabaee3a258068f2868f6692e3abf2c328112b46
balagotb <bebalago@ucsc.edu>
2016-02-05 21:40:52 -0800

    better collision
    thank ethan

--- ComaCollision/Assets/movScript.cs
+++ ComaCollision/Assets/movScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class movScript : MonoBehaviour {
 
-	const float speed = 10.0f;
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
@@ -16,9 +16,8 @@ public class movScript : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		BoxCollider2D boxC = this.gameObject.GetComponent<BoxCollider2D> ();
 		this.gameObject.GetComponent<Rigidbody2D> ().velocity = 
-			new Vector2 (Input.GetAxis ("Horizontal") * speed, Input.GetAxis ("Vertical") * speed);
+			new Vector2 (Input.GetAxisRaw ("Horizontal") * speed, Input.GetAxisRaw ("Vertical") * speed);
 
 		
 	}
