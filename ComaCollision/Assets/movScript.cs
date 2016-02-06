655ea5d91c14f3a19832b47379a713dec25d2640
Ethan <ethanmmwong@gmail.com>
2016-02-05 21:04:16 -0800

    i made collisions

---
+++ ComaCollision/Assets/movScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movScript : MonoBehaviour {
+
+	const float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		BoxCollider2D boxC = this.gameObject.GetComponent<BoxCollider2D> ();
+		this.gameObject.GetComponent<Rigidbody2D> ().velocity = 
+			new Vector2 (Input.GetAxis ("Horizontal") * speed, Input.GetAxis ("Vertical") * speed);
+
+		
+	}
+
+
+}
