1efb46caac1297daaa5041b4adf39b3d64b46be7
Kaos <kaoppenh@ucsc.edu>
2016-02-12 21:43:04 -0800

    Cherry-pick: Fixed battle for one aspect

--- COMA assets/Assets/Contact scripts/contactL.cs
+++ COMA assets/Assets/Contact scripts/contactL.cs
@@ -22,11 +22,11 @@ public class contactL : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.L)) {
 			if ( missed == 0){
-				if (Vector3.Distance(this.transform.position, source.Lscore[source.top].transform.position) < .16){
+				if (Vector3.Distance(this.transform.position, source.Lscore[source.top].transform.position) < 2.01){
 					source.kill ();
 					words.enabled = true;
 					words.text = "Good";
-				}else if (Vector3.Distance(this.transform.position, source.Lscore[source.top].transform.position)<.33){
+				}else if (Vector3.Distance(this.transform.position, source.Lscore[source.top].transform.position)<2.1){
 					source.kill ();
 					words.enabled = true;
 					words.text = "Bad";
