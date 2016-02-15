cfb0ad43b37b20d8dcc1ec1c8ae0c4d5b80332a9
Tommy <email>
2016-02-14 18:45:57 -0800

    added an "Angel" object that is global to the entire game

---
+++ COMA assets/Assets/Angel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Angel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+
+		Application.LoadLevel("title");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
