fbbf649e337e36fe1711bfe442b449b21025377a
james I <jiwamasa@ucsc.edu>
2016-02-19 20:57:07 -0800

    Start delay fix

---
+++ COMA assets/Assets/StartDelay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartDelay : MonoBehaviour {
+
+	[SerializeField]
+	public const float startDelayTime = 3f;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource audioSource = GetComponent<AudioSource> ();
+		audioSource.PlayDelayed (startDelayTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
