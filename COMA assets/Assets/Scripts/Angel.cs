29aafbef5d575a5e7c342a99f63b8b0706c1f6c1
Tommy <email>
2016-02-26 18:32:05 -0800

    tweaked the way the "Angel" script is going to work

---
+++ COMA assets/Assets/Scripts/Angel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Angel {
+
+	public static float hp;
+//	public static List<Item> inventory;
+
+//	Application.LoadLevel("title");
+
+	// Field data to be remembered during battle scenes
+	private static string field_scene_name;
+	private static Vector3 field_blue_pos;
+
+	public static void TransitionFromFieldToField(string destination) {
+		Application.LoadLevel(destination);
+	}
+	public static void TransitionFromBattleToField() {
+		Application.LoadLevel(field_scene_name);
+	}
+	public static void TransitionFromFieldToBattle() {
+//		field_scene_name = ???;
+//		field_blue_pos = ???;
+
+//		Application.LoadLevel(???);
+	}
+	public static void StartFieldScene() {
+//		??? = field_blue_pos;
+	}
+}
