c78329afb481b0b0f454fa197b6feeba4d8dc53c
Ethan <ethanmmwong@gmail.com>
2016-02-26 22:02:16 -0800

    added in save and load functions
    was testing it with Alisa's code from the google drive
    - updated it but not yet finished with handling information over scenes.

---
+++ COMA assets/Assets/Scripts/LoadSave Scripts/PlayerSaveData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[Serializable]
+class PlayerSaveData {
+
+	public string level;
+
+}
