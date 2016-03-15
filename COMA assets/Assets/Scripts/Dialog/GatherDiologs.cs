9e822c0b6a7bce73761bfd0d8c1fab254b0c8be8
alprusa <aprusa@ucsc.edu>
2016-03-15 06:10:53 -0700

    Can read from csv sheet Choice Characters still need work

---
+++ COMA assets/Assets/Scripts/Dialog/GatherDiologs.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GatherDiologs : MonoBehaviour {
+
+	Dictionary<GameObject, List<string>> dialogsInOrder = new Dictionary<GameObject, List<string>>();
+	public TextAsset csvFile;
+
+	//for quests
+	private string ACCEPT_QUEST = "Accept";
+	private string DECLINE_QUEST = "Decline";
+
+	//for converstaions
+	private string QUESTION = "Question";
+	private string WALK_AWAY = "Walk Away";
+
+	// Use this for initialization
+	void Start () {
+		ReadSpreadSheets read = new ReadSpreadSheets ();
+		read.ParseCSV (csvFile);
+
+		GameObject[] allNPC = GameObject.FindGameObjectsWithTag ("NPC");
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		Dictionary<GameObject, List<Row>> listOfCharacterDialogs = new Dictionary<GameObject, List<Row>> ();
+
+		for (int i = 0; i < allNPC.Length; i++) {
+			if(!listOfCharacterDialogs.ContainsKey(allNPC[i]))
+				listOfCharacterDialogs.Add(allNPC[i], read.FindAll_ACTOR(allNPC[i].name));
+		}
+
+		if(!listOfCharacterDialogs.ContainsKey(player))
+			listOfCharacterDialogs.Add(player, read.FindAll_ACTOR(player.name));
+
+		bool firstFound = false;
+		foreach (KeyValuePair<GameObject, List<Row>> dialog in listOfCharacterDialogs) {
+			for (int i = 0; i < dialog.Value.Count && dialog.Value.Count > 0; i++) {
+				if(dialog.Value[i].LOCATION.Equals(Application.loadedLevelName)){
+					if(dialog.Value[i].CONTEXT.Contains("1st approach")){
+					dialog.Key.GetComponent<makeText> ().dialogue.Add(dialog.Value [i].CUE);
+
+					//remove each dialog once added to the dialodsInOrder dict to reduce unneccessary searching again
+					if (!dialogsInOrder.ContainsKey (dialog.Key)) {
+						dialogsInOrder.Add (dialog.Key, new List<string>(){dialog.Value [i].CUE});
+						dialog.Value.RemoveAt (i);
+					} else {
+						dialogsInOrder [dialog.Key].Add(dialog.Value [i].CUE);
+						dialog.Value.RemoveAt (i);
+					}
+
+					firstFound = true;
+					i = 0;
+				}
+
+				if (firstFound && dialog.Value[i].LOCATION.Equals(Application.loadedLevelName)) {
+					//search string for approach if this is found then just append second
+					//if not found then check choice_type
+					//check also if value -1 if so then end checking
+					string temp = dialog.Value[i].Conversation_Path_Chain;
+
+					//check for choice paths
+					int bar = temp.IndexOf ("|");
+					if (bar != -1) {
+							/*for (int k = 0; k < temp.Length; k++) {
+								int pathNum = int.Parse (temp.Substring (0, bar));
+								if(pathNum == -1) pathNum = int.Parse (temp);
+
+								if (pathNum != -1) {
+									if (dialog.Key.GetComponent<makeText> ().dialogue < 4) {
+										dialogsInOrder [dialog.Key].Add ("CHOICE");
+										dialog.Key.GetComponent<makeText> ().dialogue.Add ("CHOICE");
+
+										if (dialog.Value [i].Choice_Type.Equals ("Quest")) {
+											dialogsInOrder [dialog.Key].Add (ACCEPT_QUEST);
+											dialogsInOrder [dialog.Key].Add (DECLINE_QUEST);
+											dialog.Key.GetComponent<makeText> ().dialogue.Add (ACCEPT_QUEST);
+											dialog.Key.GetComponent<makeText> ().dialogue.Add (DECLINE_QUEST);
+										} else if (dialog.Value [i].Choice_Type.Equals ("Converstaion")) {
+											dialogsInOrder [dialog.Key].Add (QUESTION);
+											dialogsInOrder [dialog.Key].Add (WALK_AWAY);
+											dialog.Key.GetComponent<makeText> ().dialogue.Add (QUESTION);
+											dialog.Key.GetComponent<makeText> ().dialogue.Add (WALK_AWAY);
+										}
+									}
+
+
+
+									if (dialogsInOrder [dialog.Key] [i].GameObject_Interacted_With.Equals ("Player") && listOfCharacterDialogs [player] [pathNum] != null) {
+										if (dialog.Key.GetComponent<makeText> ().path1.Count < 4) {
+											dialog.Key.GetComponent<makeText> ().path1.Add (listOfCharacterDialogs [player] [pathNum].CUE);
+										}
+										dialog.Value.RemoveAt (pathNum);
+									}
+								}
+
+								int barIndex = temp.IndexOf ('|');
+								if (barIndex == -1)
+									barIndex = 0;
+								temp = temp.Substring (barIndex, temp.Length);
+							}*/
+					}
+					
+					if(dialog.Value [i].CONTEXT.EndsWith("approach"))
+							Debug.Log (dialog.Value [i].CONTEXT);
+						dialogsInOrder [dialog.Key].Add ("SECOND");
+						dialog.Key.GetComponent<makeText> ().dialogue.Add ("SECOND");
+						dialog.Key.GetComponent<makeText> ().nextDialog.Add(listOfCharacterDialogs [dialog.Key] [i].CUE);
+						dialog.Value.RemoveAt (i);
+					}
+				}
+
+			}
+
+			if (dialogsInOrder.ContainsKey (dialog.Key) && dialog.Value.Count == 0)
+				dialogsInOrder [dialog.Key].Add("END");
+			
+			firstFound = false;
+		}
+
+		//after getting all the npc diologs get blues remaining diologs
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ COMA assets/Assets/Scripts/Dialog/ReadSpreadSheets.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq; 
+
+[SerializeField]
+public class ReadSpreadSheets 
+{
+	private List<Row> parsedRows = new List<Row>();
+
+	public void ParseCSV(TextAsset csvFile)
+	{
+		int numRows = -1;
+		/*for (int i = 0; i < csvFile.text.Length; i++) {
+			if (csvFile.text [i].Equals ('\n'))
+				numRows++;
+		}*/
+
+		//Identifier,ACTOR,CUE,CONTEXT,Conversation Path Chain,Choice Type,GameObject Interacted With,INFLECTION,LOCATION,AREA,EFFECT
+		int commaPair = 0;
+		Row row = new Row ();
+		for (int i = 0, j = 0; i < csvFile.text.Length ; i++) {
+			switch(j){
+				case 0:
+					if (csvFile.text [i].Equals (',')) {
+						row.Identifier = row.Identifier.Trim ();
+						j++;
+					}
+					else row.Identifier += csvFile.text [i];
+					break;
+				case 1:
+					if (csvFile.text [i].Equals (',')) {
+						row.ACTOR = row.ACTOR.Trim ();
+						row.ACTOR = row.ACTOR;
+						j++;
+					}
+					else row.ACTOR += csvFile.text [i];
+					break;
+			case 2:
+				string temp = row.CUE + "";
+				if ((temp.EndsWith("\",") && temp[0] == '"') || (temp.EndsWith(",") && temp[0] != '"') || temp.Contains("CUE,")) {
+						row.CUE = row.CUE.Remove (row.CUE.Length - 1);
+						row.CUE = row.CUE.Trim ();
+						j++;
+						i--;
+					} else {
+						row.CUE += csvFile.text [i];
+					}
+					break;
+				case 3:
+					if (csvFile.text [i].Equals (',')) {
+						row.CONTEXT = row.CONTEXT.Trim ();
+						j++;
+					}
+					else row.CONTEXT += csvFile.text [i];
+					break;
+				case 4:
+					if (csvFile.text [i].Equals (',')) {
+						row.Conversation_Path_Chain = row.Conversation_Path_Chain.Trim ();
+						j++;
+					}
+					else row.Conversation_Path_Chain += csvFile.text [i];
+					break;
+				case 5:
+					if (csvFile.text [i].Equals (',')) {
+						row.Choice_Type = row.Choice_Type.Trim ();
+						j++;
+					}
+					else row.Choice_Type += csvFile.text [i];
+					break;
+				case 6:
+					if (csvFile.text [i].Equals (',')) {
+						row.GameObject_Interacted_With = row.GameObject_Interacted_With.Trim ();
+						j++;
+					}
+					else row.GameObject_Interacted_With += csvFile.text [i];
+					break;
+				case 7:
+					if (csvFile.text [i].Equals (',')) {
+						row.INFLECTION = row.INFLECTION.Trim ();
+						j++;
+					}
+					else row.INFLECTION += csvFile.text [i];
+					break;
+				case 8:
+					if (csvFile.text [i].Equals (',')) {
+						row.LOCATION = row.LOCATION.Trim ();
+						j++;
+					}
+					else row.LOCATION += csvFile.text [i];
+					break;
+				case 9:
+					if (csvFile.text [i].Equals (',')) {
+						row.AREA = row.AREA.Trim ();
+						j++;
+					}
+					else row.AREA += csvFile.text [i];
+					break;
+				case 10:
+					row.EFFECT += csvFile.text [i];
+					break;
+			}
+
+			if (csvFile.text [i].Equals ('\n')) {
+				row.EFFECT = row.EFFECT.Trim ();
+				parsedRows.Add(row);
+				row = new Row ();
+				commaPair = 0;
+				j = 0;
+			}
+		}
+
+		if (parsedRows [0] != null)
+			parsedRows.RemoveAt (0);
+	}
+
+	public List<Row> getParsedRows(){
+		return parsedRows;
+	}
+
+	public List<Row> FindAll_ACTOR(string name){
+		List<Row> actorsRows = new List<Row> ();
+		for(int i = 0; i < parsedRows.Count; i++){
+			if (parsedRows [i].ACTOR.Equals (name))
+				actorsRows.Add (parsedRows [i]);
+		}
+
+		return actorsRows;
+	}
+}
