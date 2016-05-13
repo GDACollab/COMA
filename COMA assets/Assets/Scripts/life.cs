76e207046be5a3cbf0b604dc3f1280bb3dc96491
Thelonius Erickson <theoserickson@gmail.com>
2016-05-13 15:51:25 -0700

    UI Fixes Part 1

--- COMA assets/Assets/Scripts/life.cs
+++ COMA assets/Assets/Scripts/life.cs
@@ -1,34 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class life : MonoBehaviour {
 
 	public Rect lifebarRect;
-	//public Rect lifebarBGRect;
 	public Texture2D lifebar;
-	//public Texture2D lifebarBG;
-	//private Health heart;
-	//public GameObject PlayerData;
 	private float width = 700f;
 	public static life instance;
 
+	private RectTransform lifeBar;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		instance = this;
-		//heart = PlayerData.GetComponent ("Health") as Health;
-	
+		lifeBar = this.GetComponent <RectTransform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	void OnGUI(){
+	/*void OnGUI(){
 		instance.lifebarRect.width = width * (Health.hp / 100);
 		instance.lifebarRect.height = 20;
 		//instance.lifebarBGRect.width = width;
 		//instance.lifebarBGRect.height = 10;
 		//GUI.DrawTexture (lifebarBGRect, lifebarBG);
 		GUI.DrawTexture (lifebarRect, lifebar);
-	}
+	}*/
 }
