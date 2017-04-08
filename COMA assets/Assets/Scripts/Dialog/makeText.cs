0acd7cd7e85bd7407abb547e77b0b40ab2364357
James Iwamasa <jiwamasa@ucsc.edu>
2017-04-08 16:31:27 -0700

    Revert "Merge branch 'master' of https://github.com/GDACollab/COMA into beatmapping"
    This reverts commit 76d334cb570d8f60aa9202a8184905b48dfc3c12, reversing
    changes made to 1f10484e0c2acdca4e0dc6551fb6ae715288c975.

--- COMA assets/Assets/Scripts/Dialog/makeText.cs
+++ COMA assets/Assets/Scripts/Dialog/makeText.cs
@@ -14,123 +14,167 @@ using System.Collections.Generic;
  * If the player denies a quest, or otherwise needs to restart the conversation,
  * type "RESET" as the last box of that tree.*/
 
+//current issue if you are collising with more then one npc then the diolog box will not open
+
 public class makeText : MonoBehaviour
 {
 
-    new GameObject textObject;
-    new GameObject choiceObject;
-    new GameObject choiceObject2;
-    Text choice1;
-    Text choice2;
-    Image ChBG;
-    Text words;
-    Image BG;
-    public List<string> dialogue = new List<string>();
-    public List<string> path1 = new List<string>();
-    public List<string> path2 = new List<string>();
-    List<string> storage = new List<string>();
-    /*public List<string> waiting = new List<string> ();
-	public List<string> thanks = new List<string> ();
-	static int quest = 0;*/
-    static int i = 0;
-    static int j = 0;
+	public GameObject textObject;
+	private GameObject choiceObject;
+	private GameObject choiceObject2;
+	public GameObject player;
+	//public Inventory inventory;
+	public GameObject dialogCSVParserObject;
+	Text choice1;
+	Text choice2;
+	Image ChBG;
+	Text words;
+	Image BG;
+	public List<string> dialogue = new List<string> ();
+	public List<string> path1 = new List<string> ();
+	public List<string> path2 = new List<string> ();
+	public List<string> nextDialog = new List<string> ();
+	List<string> storage = new List<string> ();
+	//public List<string> waiting = new List<string> ();
+	//public List<string> thanks = new List<string> ();
+	static int quest = 0;
+	static int i = 0;
+	static int j = 0;
 
-    // Use this for initialization
-    void Start()
-    {
-        storage = dialogue;
-        textObject = GameObject.Find("Text");
-        choiceObject = GameObject.Find("Ctext1");
-        choiceObject2 = GameObject.Find("Ctext2");
-        words = textObject.GetComponent<Text>();
-        BG = textObject.GetComponentInParent<Image>();
-        choice1 = choiceObject.GetComponent<Text>();
-        ChBG = choiceObject.GetComponentInParent<Image>();
-        choice2 = choiceObject2.GetComponent<Text>();
-        words.enabled = false;
-        BG.enabled = false;
-        choice1.enabled = false;
-        choice2.enabled = false;
-        ChBG.enabled = false;
+	private bool nearCharacter = false;
+	private bool inConversation = false;
+	public bool useThisConversation = false;
 
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		storage = dialogue;
+		choiceObject = textObject.transform.GetChild(0).FindChild ("Ctext1").gameObject;
+		choiceObject2 = textObject.transform.GetChild(0).FindChild ("Ctext2").gameObject;
+		words = textObject.GetComponent<Text> ();
+		BG = textObject.GetComponentInParent<Image> ();
+		choice1 = choiceObject.GetComponent<Text> ();
+		ChBG = choiceObject.GetComponentInParent<Image> ();
+		choice2 = choiceObject2.GetComponent<Text> ();
+		words.enabled = false;
+		BG.enabled = false;
+		choice1.enabled = false;
+		choice2.enabled = false;
+		ChBG.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (nearCharacter && dialogue.Count > 0 && useThisConversation) {
+			if (choice1.enabled == false) {
+				if (words.enabled == true) {
+					words.text = dialogue [i];
+				}
+				if (Input.GetKeyDown (KeyCode.Space) && words.enabled == true) {
+					if (i < dialogue.Count)
+						i++;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (choice1.enabled == false)
-        {
-            if (words.enabled == true)
-            {
-                words.text = dialogue[i];
-            }
-            if (Input.GetKeyDown(KeyCode.Space) && words.enabled == true)
-            {
-                i++;
-                if (i > dialogue.Count - 1)
-                {
-                    words.enabled = false;
-                    BG.enabled = false;
-                }
-                if (dialogue[i].CompareTo("CHOICE") == 0)
-                {
-                    choice1.enabled = true;
-                    choice2.enabled = true;
-                    ChBG.enabled = true;
-                    i++;
-                    choice1.text = dialogue[i];
-                    i++;
-                    choice2.text = dialogue[i];
-                }
-                /*if (dialogue[i].CompareTo("QUEST") == 0){
-				 * this.quest = 1;
-				 * dialogue = waiting;
-				 * }*/
-                if (dialogue[i].CompareTo("RESET") == 0)
-                {
-                    dialogue = storage;
-                    i = 0;
-                    words.enabled = false;
-                    BG.enabled = false;
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Space))
-            {
-                words.enabled = true;
-                BG.enabled = true;
-                i = 0;
-                /*if (this.quest == 1 && inventory.questItem.obtained == true){
-					inventory.questItem.obtained = false;
-					this.quest = 2;
-					dialogue = thanks;
-				}*/
-            }
-        }
-        else {
-            if (j == 0)
-            {
-                choice1.color = new Color(176.0f / 255.0f, 160.0f / 255.0f, 254.0f / 255.0f);
-                choice2.color = new Color(124.0f / 255.0f, 110.0f / 255.0f, 195.0f / 255.0f);
-            }
-            else {
-                choice1.color = new Color(124.0f / 255.0f, 110.0f / 255.0f, 195.0f / 255.0f);
-                choice2.color = new Color(176.0f / 255.0f, 160.0f / 255.0f, 254.0f / 255.0f);
-            }
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-                j = 1;
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-                j = 0;
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                if (j == 0)
-                    dialogue = path1;
-                else
-                    dialogue = path2;
-                i = 0;
-                choice1.enabled = false;
-                choice2.enabled = false;
-                ChBG.enabled = false;
-            }
-        }
-    }
+					if (i > dialogue.Count - 1) {
+						words.enabled = false;
+						BG.enabled = false;
+						player.GetComponent<PlayerMovement> ().inDialog = false;
+					} else {
+						if (dialogue [i].CompareTo ("CHOICE") == 0) {
+							choice1.enabled = true;
+							choice2.enabled = true;
+							ChBG.enabled = true;
+							if (i < dialogue.Count)
+								i++;
+							choice1.text = dialogue [i];
+							if (i < dialogue.Count)
+								i++;
+							choice2.text = dialogue [i];
+						}
+						if (dialogue [i].CompareTo ("QUEST") == 0) {
+							quest = 1;
+							dialogue.Remove ("QUEST");
+							i = 0;
+							words.enabled = false;
+							BG.enabled = false;
+							inConversation = false;
+							player.GetComponent<PlayerMovement> ().inDialog = false;
+							//dialogue = waiting;
+						}
+						if (dialogue [i].CompareTo ("RESET") == 0) {
+							dialogue = storage;
+							i = 0;
+							words.enabled = false;
+							BG.enabled = false;
+							inConversation = false;
+							player.GetComponent<PlayerMovement> ().inDialog = false;
+						}
+						if (dialogue [i].CompareTo ("SECOND") == 0) {
+							dialogue = nextDialog;
+							i = 0;
+							words.enabled = false;
+							BG.enabled = false;
+							inConversation = false;
+							player.GetComponent<PlayerMovement> ().inDialog = false;
+							//set nextDialog to be empty so this can be checked to see if a new dialog needs to be added
+						}
+					}
+				} else if (Input.GetKeyDown (KeyCode.Space)) {
+					words.enabled = true;
+					BG.enabled = true;
+					player.GetComponent<PlayerMovement> ().inDialog = true;
+					i = 0;
+					if (quest == 1/* && inventory.questItem.obtained == true*/) {
+						//inventory.questItem.obtained = false;
+						quest = 2;
+						//dialogue = thanks;
+					}
+				}
+			} else {
+				if (j == 0) {
+					choice1.color = new Color (176.0f / 255.0f, 160.0f / 255.0f, 254.0f / 255.0f);
+					choice2.color = new Color (124.0f / 255.0f, 110.0f / 255.0f, 195.0f / 255.0f);
+				} else {
+					choice1.color = new Color (124.0f / 255.0f, 110.0f / 255.0f, 195.0f / 255.0f);
+					choice2.color = new Color (176.0f / 255.0f, 160.0f / 255.0f, 254.0f / 255.0f);
+				}
+				if (Input.GetKeyDown (KeyCode.DownArrow))
+					j = 1;
+				if (Input.GetKeyDown (KeyCode.UpArrow))
+					j = 0;
+				if (Input.GetKeyDown (KeyCode.Space)) {
+					if (choice1.enabled && choice2.enabled) {
+						if (j == 0) {
+							if (dialogue != path1)
+								dialogue = path1;
+							else
+								dialogue = nextDialog;
+						} else
+							dialogue = path2;
+					} else {
+						dialogue = nextDialog;
+					}
+					i = 0;
+					choice1.enabled = false;
+					choice2.enabled = false;
+					ChBG.enabled = false;
+					//useThisConversation = false;
+				}
+			}
+		}
+	}
+
+	public void OnTriggerEnter2D (Collider2D obj)
+	{
+		if (obj.tag.Equals ("Player") && obj.isTrigger)
+			nearCharacter = true;
+	}
+
+	public void OnTriggerExit2D (Collider2D obj)
+	{
+		if (obj.tag.Equals ("Player") && obj.isTrigger) {
+			nearCharacter = false;
+			obj.GetComponent<PlayerMovement> ().inDialog = false;
+		}
+	}
 }
